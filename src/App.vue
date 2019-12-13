9146b588e7532e2c033a67fb7c53b94b1758b7f8
Ossama <ossama.attaleb@gmail.com>
2019-12-13 23:26:29 +0100

    structure the files and folders inside the Vue.js project

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
-  <div id="app">
-
-    <img alt="Vue logo" src="./assets/logo.png">
-
-
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <app-header></app-header>
+        <div class="row">
+            <index></index>
+        </div>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import './assets/css/tailwind.css';
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import Header from './components/Header.vue';
+    import Footer from './components/Footer.vue';
+    import Index from './pages/Index.vue';
+
+    import '@/assets/css/styles.css';
+    import '@/assets/css/tailwind.css';
+    
+
+    export default {
+        components: {
+            appHeader: Header,
+            'index': Index,
+            'app-footer': Footer
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="container footer-bg p-8">
+        <div class="sm:flex mb-4">
+
+             <!-- Product -->
+            <div class="sm:w-1/4 h-auto">
+                <div class="text-orange mb-2">Product</div>
+                <ul class="list-reset leading-normal">
+                    <li class="text-grey-darker">One</li>
+                    <li class="text-grey-darker">Two</li>
+                    <li class="text-grey-darker">Three</li>
+                    <li class="text-grey-darker">Four</li>
+                </ul>
+            </div>
+
+
+            <!-- Company -->
+            <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
+                <div class="text-blue mb-2">Company</div>
+                <ul class="list-reset leading-normal">
+                    <li class="hover:text-blue text-grey-darker">One</li>
+                    <li class="hover:text-blue text-grey-darker">Two</li>
+                    <li class="hover:text-blue text-grey-darker">Three</li>
+                    <li class="hover:text-blue text-grey-darker">Three</li>
+                </ul>
+            </div>
+
+
+            <!-- Help Center -->
+            <div class="sm:w-1/4 h-auto sm:mt-0 mt-8">
+                <div class="text-green-dark mb-2">Green-dark</div>
+                <ul class="list-reset leading-normal">
+                    <li class="hover:text-green-dark text-grey-darker">One</li>
+                    <li class="hover:text-green-dark text-grey-darker">Two</li>
+                    <li class="hover:text-green-dark text-grey-darker">Three</li>
+                </ul>
+            </div>
+
+
+            <!-- Stay in touch -->
+            <div class="sm:w-1/2 sm:mt-0 mt-8 h-auto">
+                <div class="text-red-light mb-2">Stay in touch</div>
+                <p class="text-grey-darker leading-normal">Join our mailing list to get update. We respect your privacy.  </p>
+                
+
+                <div class="mt-4 flex">
+                    <input type="text" class="p-2 border border-grey-light round text-grey-dark text-sm h-auto" placeholder="Your email address">
+                    <button class="bg-orange text-white rounded-sm h-auto text-xs p-3">Subscribe</button>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Copyright -->
+        <div class="sm:flex mt-4">
+            <!-- logo -->
+            <div class="sm:w-1/4 h-auto">
+                <div class="text-orange mb-2">logo</div>
+            </div>
+
+             <div class="sm:w-2/4 h-auto sm:mt-0 mt-8">
+                <div class="text-red-light mb-2">Stay in touch</div>
+             </div>
+
+             <div class="sm:w-1/2 sm:mt-0 mt-8 h-auto">
+                logo facebook
+             </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
